8786fa41c0892b8f5b0fef5420522e6e53a7080e
Edo Sani <sani140402@gmail.com>
2023-03-27 23:10:58 +0700

    u

--- index.js
+++
@@ -1,5 +0,0 @@
-// Mendapatkan URL lengkap (termasuk domain) dari file .html saat ini
-var currentUrl = window.location.href;
-
-// Redirect ke file .html dengan URL yang ditentukan
-window.location.href = "Peta Kab. Gorontalo.html";
